c511ceccd0db62f95676ec904e9ee894c8625223
Alireza <alirezashadow52555@gmail.com>
2023-03-16 14:00:05 +0330

    ad getbalance contract

---
+++ GetBalance.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+
+/// @title This contract will get balance of ERC20 token of entered address. also can get balances in the ERC20 contract.
+/// @author Alireza Haghshenas
+/// @notice This contract is just for learning to develop smart contract and to get balance and allowance there is more simple ways.
+/// @dev Explain to a developer any extra details
