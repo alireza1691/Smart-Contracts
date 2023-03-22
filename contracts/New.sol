cd43022b0300350b0a96fca9d7900f8bba1ac953
Alireza <alirezashadow52555@gmail.com>
2023-03-22 17:04:20 +0330

    ERC20

--- contracts/New.sol
+++ contracts/New.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.18;
 
-contract New {
-    constructor() {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract New is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20( name, symbol ) {
         
     }
+
+
+
+
+
 }
