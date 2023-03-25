43dec1834ed72ad239f387d60285e250198f5394
Alireza <alirezashadow52555@gmail.com>
2023-03-25 15:29:15 +0330

    information

--- contracts/New.sol
+++ contracts/New.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/// @title A title that should describe the contract/interface
+/// @author Alireza Haghshenas
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
+
 contract New is ERC20 {
     constructor(string memory name, string memory symbol) ERC20( name, symbol ) {
         
