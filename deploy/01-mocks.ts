7f54c6e16f07034348bb32933c55e8efafd4aa4a
Alireza <alirezashadow52555@gmail.com>
2023-03-29 14:29:17 +0330

    delete extra files

--- deploy/01-mocks.ts
+++ deploy/01-mocks.ts
@@ -1,19 +1,9 @@
 const { log } = require('console');
 const { ethers, newtwork } = require('hardhat')
 import { DeployFunction } from "hardhat-deploy/types"
-// const { DeployFunction } = require('hardhat-deploy/types')
 
 
 
-
-// module.exports = async () => {
-
-//     const accounts = await ethers.getSigners()
-//     const token = await ethers.getContractFactory("Token")
-//     console.log(token);
-//     const instance = await token.deploy("name","symbol")
-//     await instance.deployed()
-// }
 
 const deployMocks: DeployFunction = async function () {
     const accounts = await ethers.getSigners()
